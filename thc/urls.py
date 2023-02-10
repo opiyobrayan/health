cedb6d3ab5a9292178bac57bac20f3aa4a04fb77
opiyobrayan <67815971+opiyobrayan@users.noreply.github.com>
2023-02-10 15:55:19 +0300

    first commit

--- thc/urls.py
+++ thc/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     
     path('workplan', views.workplan,name='workplan'),
     path('pdf/<int:pk>', views.view_pdf, name='view_pdf'),
-    path('excel', views.display_excel, name='excel'),
+
     path('activity', views.activity, name='activity'),
     path('add-activity', views.add_activity, name='add-activity'),
     path('partners', views.partners, name='partners'),
--- thc/views.py
+++ thc/views.py
@@ -26,19 +26,7 @@ def view_pdf(request, pk):
 
 # views.py
 from django.shortcuts import render
-import openpyxl
 
-def display_excel(request):
-    workplan = Workplan.objects.get(pk=1)
-    wb = openpyxl.load_workbook(workplan.pdf.path)
-    sheet = wb.active
-    data = []
-    for row in sheet.iter_rows():
-        data_row = []
-        for cell in row:
-            data_row.append(cell.value)
-        data.append(data_row)
-    return render(request, 'display_excel.html', {'data': data})
 def activity(request):
     return render(request, 'activity.html', {})
 def add_activity(request):
